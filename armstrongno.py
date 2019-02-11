b8375e6e06fc3ae609f9f01411268d4f0005e474
VISALI-220299 <46592753+VISALI-220299@users.noreply.github.com>
2019-02-11 01:10:23 -0800

    Update armstrongno.py

--- armstrongno.py
+++ armstrongno.py
@@ -1,11 +1,11 @@
 a=int(input())
 b=int(input())
 for i in range(a,b+1):
-	sum=0
-	temp=i
-	while(temp>0):
-		digit=temp%10
-		sum=sum+digit**3
-		temp=temp//10
-		if(i==sum):
+	s=0
+	t=i
+	while(t>0):
+		d=t%10
+		s=s+d**3
+		t=t//10
+		if(i==s):
 			print(i)
