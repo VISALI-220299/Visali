2932535f0219b445a0be9afae00ac2e7c8f23151
VISALI-220299 <46592753+VISALI-220299@users.noreply.github.com>
2019-02-28 22:08:03 +0530

    Update pro14.py

--- pro14.py
+++ pro14.py
@@ -5,10 +5,10 @@ a=[]
 for i in range(0,k):
 		li.append(int(input()))
 		for i in range(0,v):
-			m=0
-			s=int(input())
-			p=int(input())
+		m=0
+		s=int(input())
+		p=int(input())
 		for i in range(s-1,p):
 			m=m^li[i]
-			a.append(m)
+		a.append(m)
 print(a)
