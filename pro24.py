de5fe5e20bf984e077141ea56c89c83f8ccd59a9
VISALI-220299 <46592753+VISALI-220299@users.noreply.github.com>
2019-06-12 19:58:17 +0530

    Update pro24.py

--- pro24.py
+++ pro24.py
@@ -1,13 +1,13 @@
 num=int(input())
-a=[]
+v=[]
 k=bin(2**num-1)[2::]
 l=len(k)
 for i in range(0,2**num):
 	s=bin(i)[2::]
 	if len(s)<l:
-		a.append([s.count("1"),(l-len(s))*"0"+s])
+		v.append([s.count("1"),(l-len(s))*"0"+s])
 	else:
-		a.append([s.count("1"),s])
-a.sort()
-for i in range(0,len(a)):
-	print(a[i][1])
+		v.append([s.count("1"),s])
+v.sort()
+for i in range(0,len(v)):
+	print(v[i][1])
