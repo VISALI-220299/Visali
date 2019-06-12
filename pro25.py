ebf23a624172413d4a2f2ab1f07de6db7f169ec3
VISALI-220299 <46592753+VISALI-220299@users.noreply.github.com>
2019-06-12 20:00:39 +0530

    Create pro25.py

---
+++ pro25.py
@@ -0,0 +1,12 @@
+num1=int(input())
+l1=list(map(int,input().split()))
+m1=[]
+a1=1
+for i in range(num1-1):
+    if l1[i]<l1[i+1]:
+        a1+=1
+    else:
+        m1.append(a1)
+        a1=1
+m1.append(a1)
+print(max(m1))
